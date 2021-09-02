2a32b0d90f0ee07d75e97f27c08b51f3a3ae1e32
Valera <pomytkina_76@mail.ru>
2021-09-02 11:11:34 +0300

    step12turn1

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,21 @@
 "use strict";
 
-const category = 'toys';
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-console.log(`https://someurl.com/${category}`);
+const personalMOvieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    generes: [],
+    private: false
+};
 
-const user = 'Ivan';
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
 
-alert(`Privet, ${user}`);
+personalMOvieDB.movies[a] = b;
+personalMOvieDB.movies[c] = d;
+
+console.log(personalMOvieDB);
